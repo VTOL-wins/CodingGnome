c62f673834c5960adc7edbd384748eb164946cc4
Nathan Asberry <nathan.asberry@binarynoggin.com>
2018-04-06 12:18:24 -0500

    Game Graphics added.
    The majority of the web handling and templates have been created.
      * having two tabs open while hosting locally is sharing states
        currently
      * There is a small bug in the display of the graphic upon game loss
    
    A new graphic has been included on the game page that will display body
    parts being filled in as you make bad guesses.
    
    Nathan Asberry @VTOL-wins <nathan.asberry@binarynoggin.com>

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,28 +1,22 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
-  @responses %{
-    :won          => { :success, "You Won!" },
-    :lost         => { :danger, "You Lost!" },
-    :good_guess   => { :success, "Good guess!" },
-    :bad_guess    => { :warning, "Bad guess!" },
-    :already_used => { :info, "You already guessed that."},
-  }
+  import GallowsWeb.Views.Helpers.GameStateHelper
 
-  def game_state(state) do
-    @responses[state]
-    |> alert()
+  def turn(left, target) when target >= left do
+    "opacity: 1"
   end
 
-  defp alert(nil), do: ""
+  def turn(left, target) do
+    "opacity: 0.1"
+  end
 
-  defp alert({class, message}) do
-    """
-    <div class="alert alert-#{class}">
-      #{message}
-    </div>
-    """
-    |> raw()
+  def new_game_button(conn) do
+    button("New Game", to: hangman_path(conn, :create_game))
+  end
+
+  def game_over?( %{ game_state: game_state}) do
+    game_state in [ :won, :lost ]
   end
 
 end
